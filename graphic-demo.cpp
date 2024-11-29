684b9b105f8d1d3ee7851423b277432977de93bd
Hayden Clark <hayden.clark@mymail.champlain.edu>
2024-11-29 02:19:32 -0500

    Simplify memory management

--- graphic-demo.cpp
+++ graphic-demo.cpp
@@ -5,7 +5,7 @@ const float SCALE_FACTOR = 100;
 const int WINDOW_HEIGHT = 600;
 const int WINDOW_WIDTH = 800;
 
-std::vector<sf::Sprite *> itemsToDraw = {};
+std::vector<sf::Sprite> itemsToDraw = {};
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Stack Demo", sf::Style::Close);
@@ -28,7 +28,7 @@ int main() {
         window.draw(sprite);
 
         for (const auto &item: itemsToDraw) {
-            window.draw(*item);
+            window.draw(item);
         }
         window.display();
 
@@ -36,18 +36,16 @@ int main() {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
+                // needed to ensure that the sprites are placed correctly. doing setPosition(mouseX, mouseY) doesn't align them to the correct spots.
                 int scaledX = event.mouseButton.x / SCALE_FACTOR;
                 int scaledY = event.mouseButton.y / SCALE_FACTOR;
 
-                sf::Sprite *newSprite = new sf::Sprite;
-                newSprite->setTexture(texture);
-                newSprite->setPosition(scaledX * SCALE_FACTOR, scaledY * SCALE_FACTOR);
+                sf::Sprite newSprite;
+                newSprite.setTexture(texture);
+                newSprite.setPosition(scaledX * SCALE_FACTOR, scaledY * SCALE_FACTOR);
                 itemsToDraw.push_back(newSprite);
             }
             if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Escape) {
-                for (auto *item: itemsToDraw) {
-                    delete item;
-                }
                 itemsToDraw.clear();
             }
             if (event.type == sf::Event::Closed) {
